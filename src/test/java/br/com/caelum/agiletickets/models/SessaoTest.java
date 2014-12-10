1304bdfdbe7ad9a61640daa20bd76677ce1eea9a
andre <torquato.desenv@gmail.com>
2014-12-10 09:25:31 -0200

    corrige bug sessao vendas de ingressos

--- src/test/java/br/com/caelum/agiletickets/models/SessaoTest.java
+++ src/test/java/br/com/caelum/agiletickets/models/SessaoTest.java
@@ -14,6 +14,14 @@ public class SessaoTest {
 	}
 	
 	@Test
+	public void deveVender5ingressoSeHa5vagas() throws Exception {
+		Sessao sessao = new Sessao();
+        sessao.setTotalIngressos(5);
+
+        Assert.assertTrue(sessao.podeReservar(5));
+	}
+	
+	@Test
 	public void deveVender5ingressosSeHa10vagas() throws Exception {
 		Sessao sessao = new Sessao();
 		sessao.setTotalIngressos(10);
